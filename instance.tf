af98cd1fd928ba4276489508035966a33158b17c
yehara <yoshinori.ehara@gmail.com>
2017-01-13 13:46:32 +0900

    web1 インスタンスを追加

---
+++ instance.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "web1" {
+
+    ami                         = "ami-9f0c67f8"
+    availability_zone           = "ap-northeast-1b"
+    ebs_optimized               = false
+    instance_type               = "t2.micro"
+    monitoring                  = false
+    key_name                    = "mijs-key"
+    subnet_id                   = "${aws_subnet.vpc1-subnet.0.id}"
+    vpc_security_group_ids      = ["${aws_security_group.vpc-default.id}"]
+    source_dest_check           = true
+
+    root_block_device {
+        volume_type           = "gp2"
+        volume_size           = 8
+        iops                  = 100
+        delete_on_termination = true
+    }
+
+    tags {
+        "Name" = "web1"
+    }
+}
+
+output "web1-address" {
+  value = "${aws_instance.web1.public_dns}"
+}
+
