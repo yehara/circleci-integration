0aea13cb6c19e651db8ae645aeb3c1896cc62931
yehara <yoshinori.ehara@gmail.com>
2017-01-13 16:48:20 +0900

    ELB を追加

---
+++ elb.tf
@@ -0,0 +1,31 @@
+resource "aws_elb" "web-elb" {
+  name = "web-elb"
+  subnets = [
+    "${aws_subnet.vpc1-subnet.0.id}",
+    "${aws_subnet.vpc1-subnet.1.id}",
+  ]
+  security_groups = [
+    "${aws_security_group.vpc-default.id}",
+  ]
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    target = "HTTP:80/"
+    interval = 30
+  }
+  instances = ["${aws_instance.web1.id}", "${aws_instance.web2.id}"]
+  cross_zone_load_balancing = true
+  connection_draining = true
+  connection_draining_timeout = 400
+  tags {
+    Name = "web-elb"
+  }
+}
+
--- output.tf
+++ output.tf
@@ -6,4 +6,7 @@ output "web2-address" {
   value = "${aws_instance.web2.public_dns}"
 }
  
+output "web-elb-address" {
+  value = "${aws_elb.web-elb.dns_name}"
+}
 
